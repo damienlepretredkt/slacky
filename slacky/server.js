0757353512895a530c7e3da3565f7b4b056b5860
Damien LEPRETRE <damien.lepretre@decathlon.com>
2018-01-16 11:47:22 +0100

    clean code

--- slacky/server.js
+++ slacky/server.js
@@ -5,15 +5,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: isPgSslActive()
-// });
-
 app.use(express.static(path.join(__dirname, 'build')));
-
 
 app.use(require("body-parser").urlencoded({ extended: true }));
 app.use(require("cookie-parser")());
@@ -35,17 +27,6 @@ wss.on("connection", (ws, req) => {
 
   ws.on('error', (err) => console.log(err));
 
-  // When a user quit, we send the information to all users
-  // ws.on("message", (data) => {
-  //   if (data === "CLOSE") {
-  //     numberOfUsers -= 1;
-  //     wss.clients.forEach((client) => {
-  //       if (client.readyState === WebSocket.OPEN) {
-  //         client.send(numberOfUsers);
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 
